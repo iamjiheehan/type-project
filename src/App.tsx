f217a780daef60743c9515bd2d95d2c76bfcab5f
한지희, Jihee Han <hatrix1014@gmail.com>
2023-04-18 01:49:29 +0900

    디자인 패턴 생성

--- src/App.tsx
+++ src/App.tsx
@@ -4,22 +4,25 @@ import './App.css';
 
 function App() {
 
+  interface Student {
+    name : string;
+  }
+
+  interface Student {
+    score : number;
+  }
+
+  const student : Student = {
+    name : "John",
+    score : 100,
+  }  
+
+  console.log(student);
+  
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      
     </div>
   );
 }
